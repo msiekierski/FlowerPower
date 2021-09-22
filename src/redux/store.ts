ffa4506803c2a2687ab7e6ba6c6bb389e9218cb8
Mateusz Siekierski <mateuszsen99@gmail.com>
2021-09-22 07:29:10 +0200

    add redux dev tools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,20 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import persistStore from 'redux-persist/es/persistStore';
 import thunk from 'redux-thunk';
 import { pReducer } from './root-reducer';
 
-export const store = createStore(pReducer, {}, applyMiddleware(thunk));
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  pReducer,
+  {},
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export const persistor = persistStore(store);
