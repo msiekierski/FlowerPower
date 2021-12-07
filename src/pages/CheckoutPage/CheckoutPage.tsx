eafdfbaf1791bfd3a46e67dc632bafcf3d982b83
Mateusz Siekierski <mateuszsen99@gmail.com>
2021-12-07 04:29:11 +0100

    .

--- src/pages/CheckoutPage/CheckoutPage.tsx
+++ src/pages/CheckoutPage/CheckoutPage.tsx
@@ -229,9 +229,8 @@ const CheckoutPage = () => {
                   Thank you for your order.
                 </Typography>
                 <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order
-                  confirmation, and will send you an update when your order has
-                  shipped.
+                  We have emailed your order confirmation, and will send you an
+                  update when your order has shipped.
                 </Typography>
               </React.Fragment>
             ) : (
